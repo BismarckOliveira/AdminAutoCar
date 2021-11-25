3f8fc11fc199c40bdd3739747850b63cecc53fb3
Bismarck Oliveira <bismarckoliver@hotmail.com>
2021-11-25 16:31:44 -0300

    feat: 🎸 Adiciona serviço de import e documentaçao da api
    Adiciona serviço de import e documentaçao da api

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 const categoriesRoute = Router();
 categoriesRoute.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
+});
+
+categoriesRoute.post("/import", upload.single("file"), (request, response) => {
+  importCategoryController.handle(request, response);
 });
 
 categoriesRoute.get("/", (request, response) => {
