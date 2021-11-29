ab8d28584a6bb52d6946a3fd9846f3e6975f9d06
Bismarck Oliveira <bismarckoliver@hotmail.com>
2021-11-29 01:36:36 -0300

    feat: 🎸 Adicionad Criacao de carro
    Adicionad Criacao de carro

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -0,0 +1,51 @@
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "../../../../errors/AppError";
+import { ICarsRepository } from "../../repositories/ICarsRepository";
+
+interface IResquest {
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+}
+
+@injectable()
+class CreateCarUseCase {
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository
+  ) {}
+  async execute({
+    name,
+    brand,
+    category_id,
+    daily_rate,
+    description,
+    fine_amount,
+    license_plate,
+  }: IResquest): Promise<void> {
+    const carAlreadyExists = await this.carsRepository.findByLicensePlate(
+      license_plate
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError("Car already exists!");
+    }
+
+    await this.carsRepository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+  }
+}
+
+export { CreateCarUseCase };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
+import { carRoutes } from "./cars.routes";
 import { categoriesRoute } from "./categories.routes";
 import { especificationRouter } from "./specifications.routes";
 import { userRoutes } from "./users.routes";
@@ -8,6 +9,7 @@ import { userRoutes } from "./users.routes";
 const router = Router();
 
 router.use("/users", userRoutes);
+router.use("/cars", carRoutes);
 router.use("/categories", categoriesRoute);
 router.use("/specifications", especificationRouter);
 router.use(authenticateRoutes);
