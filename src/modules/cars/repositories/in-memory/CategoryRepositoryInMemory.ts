3f6f10b6b1a1e7637ec5714ab1b228f35be917d9
Bismarck Oliveira <bismarckoliver@hotmail.com>
2022-01-09 14:07:42 -0300

    Create Repository In memory for tests

---
+++ src/modules/cars/repositories/in-memory/CategoryRepositoryInMemory.ts
@@ -0,0 +1,47 @@
+import { Category } from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+  IUpdateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoryRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[] = [];
+
+  async create({ description, name }: ICreateCategoryDTO): Promise<void> {
+    this.categories.push({
+      name,
+      description,
+      created_at: new Date(),
+    });
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find((category) => category.name === name);
+  }
+  async findById(id: string): Promise<Category> {
+    return this.categories.find((category) => category.id === id);
+  }
+  async remove(category: Category): Promise<void> {
+    const index = this.categories.indexOf(category);
+    this.categories.splice(index, 1);
+  }
+
+  async updateName({ id, name }: IUpdateCategoryDTO): Promise<void> {
+    const category = this.categories.find((category) => category.id === id);
+    const index = this.categories.indexOf(category);
+    const categoryUpdated = {
+      id: category.id,
+      name,
+      description: category.description,
+      created_at: category.created_at,
+    };
+    this.categories.splice(index, 1, categoryUpdated);
+  }
+}
+
+export { CategoryRepositoryInMemory };
