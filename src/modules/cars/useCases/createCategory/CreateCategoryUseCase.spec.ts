45a4b2870655c34352e4f5a385844e570615d4c0
Bismarck Oliveira <bismarckoliver@hotmail.com>
2022-01-09 14:06:30 -0300

    create primary test

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,8 @@
+describe("Criar Categoria", () => {
+  it("Espero que 2 + 2 Seja Igual a 4", () => {
+    const soma = 2 + 2;
+    const resultado = 4;
+
+    expect(soma).toBe(resultado);
+  });
+});
