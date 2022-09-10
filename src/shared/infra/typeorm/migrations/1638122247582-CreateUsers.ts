d4eb2fd83186727034a50d490e6ff597b3f1f29e
Bismarck Oliveira <bismarckoliver@hotmail.com>
2022-09-10 18:06:57 -0300

    add migration rentals

--- src/shared/infra/typeorm/migrations/1638122247582-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1638122247582-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1638122247582 implements MigrationInterface {
           {
             name: "id",
             type: "uuid",
+            isPrimary: true,
           },
           {
             name: "name",
