04789da3c941a68426c8129526d26299f6d8820f
Bismarck Oliveira <bismarckoliver@hotmail.com>
2022-09-10 23:27:47 -0300

    add create rental endpoint

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.ts
@@ -1,5 +1,7 @@
 /* eslint-disable prettier/prettier */
 
+import { inject, injectable } from "tsyringe";
+
 import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
 import { IRentalsRepository } from "@modules/rentals/repositories/IRentalsRepository";
 import { IDateProvider } from '@shared/container/providers/DateProvider/IDateProvider';
@@ -12,9 +14,12 @@ interface IRequest {
   expected_return_date: Date;
 }
 
+@injectable()
 class CreateRentalUseCase {
   constructor(
+    @inject('RentalsRepository')
     private rentalsRepository: IRentalsRepository,
+    @inject('DayjsDateProvider')
     private dateProvider: IDateProvider
   ) { }
 
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { carsRoutes } from "./cars.routes";
 import { categoriesRoute } from "./categories.routes";
+import { rentalRoutes } from "./rental.routes";
 import { especificationRouter } from "./specifications.routes";
 import { userRoutes } from "./users.routes";
 
@@ -12,6 +13,8 @@ router.use("/users", userRoutes);
 router.use("/cars", carsRoutes);
 router.use("/categories", categoriesRoute);
 router.use("/specifications", especificationRouter);
+router.use("/rental", rentalRoutes);
+
 router.use(authenticateRoutes);
 
 export { router };
